31a4ffdba35ea53542ea466ebc2ce8c42902895b
Kaua-padilha16 <kaua.4904@aluno.pr.senac.br>
2024-03-08 10:52:01 -0300

    Criação das tabelas no banco de dados, usando as migrations

--- src/database/knex/migrations/20240308124019_createUsers.js
+++ src/database/knex/migrations/20240308124019_createUsers.js
@@ -1,15 +1,15 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+
+exports.up = (knex) => {
+    return knex.schema.createTable("users", (table) => {
+        table.increments('id').primary();
+        table.string("name").notNullable();
+        table.string("email").notNullable();
+        table.string("password").notNullable();
+        table.string("fone").notNullable();
+        table.timestamp("dateCreate").defaultTo(knex.fn.now());
+    })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+exports.down = (knex) => {
+    return knex.schema.dropTableIfExists("users");
 };
---
+++ src/database/knex/migrations/20240308132101_createBooks.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("books", (table) => {
+        table.increments('id').primary();
+        table.string("title").notNullable();
+        table.string("author").notNullable();
+        table.string("numPag").notNullable();
+        table.string("category").notNullable();
+        table.boolean("available").defaultTo("false");
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("books")
+};
