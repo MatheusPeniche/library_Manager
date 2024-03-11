5991a3161ada472eacd442830ba284ad33ee3ba0
MatheusPeniche <peniche.9988@aluno.pr.senac.br>
2024-03-11 09:35:17 -0300

    Esses é o nome arquivo que eu peguei com o nome do Kauã e aquele q eu fiz teve que ser deletado por um erro que deu na migrations. Enquanto eu mexia aqui no novo projeto que agora vai ser salvo com meu nome, foi feita novas configurações no migrations, no routes e no UserController.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,9 @@
 const {Router} = require("express");
 const bookRoutes = require("./book.routes");
-//const userRoutes = require("./user.routes");
+const userRoutes = require("./user.routes");
 const routes = Router()
 
 routes.use("/", bookRoutes)
-//routes.use("/", userRoutes)
+routes.use("/", userRoutes)
 
 module.exports = routes
